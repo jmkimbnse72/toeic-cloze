da13d64bf0daafd328bee505d439423e14a13b81
jmkimbnse72 <255271701+jmkimbnse72@users.noreply.github.com>
2026-06-15 09:14:56 +0900

    메모: 펜 전용, 스무딩, 손가락 스크롤

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.63li9qjobm8"
+    "revision": "0.kcbu56tjegs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
